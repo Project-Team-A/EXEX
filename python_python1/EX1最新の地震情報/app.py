879083c2318825ab768034b175ddc8c080d746dc
nakamura <c2p31081@bunkyo.ac.jp>
2023-11-20 16:21:38 +0900

    main

--- python_python1/EX1最新の地震情報/app.py
+++ python_python1/EX1最新の地震情報/app.py
@@ -1,40 +1,35 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
-import threading
 import requests
 
 app = Flask(__name__)
-socketio = SocketIO(app)
-
-# 地震情報取得関数
-def get_earthquake_info():
-    p2pquake_url = 'https://api.p2pquake.net/v2/history?codes=551&limit=1'
-
-    try:
-        response = requests.get(p2pquake_url)
-        response.raise_for_status()
-        earthquake_info = response.json()[0]  # 最新の地震情報を取得
-        return earthquake_info
-    except requests.RequestException as e:
-        print(f"Error fetching earthquake data: {e}")
-        return None
-
-# 地震情報をクライアントに送信する関数
-def send_earthquake_info():
-    while True:
-        info = get_earthquake_info()
-        if info:
-            magnitude = info.get("earthquake", {}).get("magnitude")
-            if magnitude and magnitude >= 5.0: #マグニチュード
-                socketio.emit('new_earthquake', {'magnitude': magnitude})
-        socketio.sleep(30) #30秒ごとに
-
-# 地震情報送信用スレッドの開始
-threading.Thread(target=send_earthquake_info, daemon=True).start()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # 地震情報のAPIエンドポイント
+    p2pquake_url = 'https://api.p2pquake.net/v2/history?codes=551&limit=1'
+    
+    # APIから地震情報を取得
+    try:
+        response = requests.get(p2pquake_url)
+        response.raise_for_status()  # ステータスコードが200以外の場合はエラーを発生させる
+        earthquake_info = response.json()[0]  # 最新の地震情報を取得
+    except requests.RequestException as e:
+        earthquake_info = None
+        print(f"Error fetching earthquake data: {e}")
+
+    # 地震情報が取得できたかチェック
+    if earthquake_info:
+        # 震央地名
+        eq_name = earthquake_info["earthquake"]["hypocenter"]["name"]
+        # 震度
+        max_intensity = earthquake_info["earthquake"]["maxScale"]
+        # 日時
+        eq_datetime = earthquake_info["earthquake"]["time"]
+    else:
+        eq_name = max_intensity = eq_datetime = "データが取得できませんでした"
+
+    # 地震情報をHTMLテンプレートに渡してレンダリング
+    return render_template('index.html', eq_name=eq_name, max_intensity=max_intensity, eq_datetime=eq_datetime)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(debug=True)
